410def00f02c8ee72004bf93065351357819fae9
unknown <Digital@DH-A4-38.dhbrasil.digitalhouse.com>
2019-03-30 11:39:28 -0300

    Adicionado funcionalidades de admin no menu

--- index.php
+++ index.php
@@ -9,6 +9,14 @@ $descricaoCurso2 = "Curso de Marketing";
 $valorCurso2 = 1000.98;
 $imagemCurso2 = "marketing.jpg";
 
+$usuario = [
+  "Nome" => "Thomaz",
+  "Email" => "teste@teste.com",
+  "Senha" => "123456",
+  "NivelAcesso" => mt_rand(0,1)
+  
+];
+
 ?>
 
 
@@ -26,7 +34,7 @@ $imagemCurso2 = "marketing.jpg";
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
 
 <!-- Latest compiled and minified JavaScript -->
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+<link rel="stylesheet" href="assets/css/style.css">
 </head>
 <body>
     <nav class="navbar navbar-default">
@@ -36,19 +44,46 @@ $imagemCurso2 = "marketing.jpg";
            <span>Cursos</span>
         </a>
         </div>
+
+      <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+        <?php if ($usuario ["NivelAcesso"]==1) : ?>
+              <ul class="nav navbar-nav">
+                <li class="dropdown">
+                  <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Ações <span class="caret"></span></a>
+                  <ul class="dropdown-menu">
+                    <li><a href="#">Adicionar Produto</a></li>
+                    <li><a href="#">Editar Produto</a></li>
+                  </ul>
+                </li>
+              </ul>
+                
+     
+
+        <?php endif;?>
+      
+      
     <form class="navbar-form navbar-left" role="search">
   <div class="form-group">
     <input type="text" class="form-control" placeholder="Pesquise Aqui">
   </div>
-  <button type="submit" class="btn btn-default">BUSCAR</button>
-</form>
+  <button type="submit" class="btn btn-default"><span class="glyphicon glyphicon-search" aria-hidden="true"></span></button>
+  </form>
+  <p class="navbar-text navbar-right">
+  Logado como
+  <strong>
+  <a href="#" class="navbar-link"> <?php echo $usuario ["Nome"];?> </a>
+  </strong>
+  </p>
+
+
+</div>
     </div>
     </nav>
 
 <div class="container">
 <div class="row">
 
-  <div class="col-sm-6 col-md-4">
+  <div class="col-sm-6 col-md-6">
     <div class="thumbnail">
       <img src="<?php  echo "assets/img/$imagemCurso1"; ?>" alt="<?php  echo  "Foto Curso $nomeCurso1"; ?>">
       <div class="caption">
@@ -59,7 +94,7 @@ $imagemCurso2 = "marketing.jpg";
       </div>
     </div>
   </div>
-  <div class="col-sm-6 col-md-4">
+  <div class="col-sm-6 col-md-6">
     <div class="thumbnail">
       <img src="<?php  echo "assets/img/$imagemCurso2"; ?>" alt="<?php  echo  "Foto Curso $nomeCurso2"; ?>">
       <div class="caption">
@@ -73,7 +108,9 @@ $imagemCurso2 = "marketing.jpg";
 </div>
 </div>
 
- 
+ <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
     
 </body>
 </html>
