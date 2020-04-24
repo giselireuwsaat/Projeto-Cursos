8a86191e32c8edc3c605e810ab3c671ec6470eb3
Giseli Vasconcelos Reuwsaat <45762730+giselireuwsaat@users.noreply.github.com>
2020-04-24 20:49:12 -0300

    fix: database information

--- req/database.php
+++ req/database.php
@@ -1,5 +1,5 @@
 <?php
-	$dsn = 'https://git.heroku.com/projeto-cursos.git';
+	$dsn = 'mysql:host=https://git.heroku.com/projeto-cursos.git;dbname=us-cdbr-iron-east-01.cleardb.net;charset=utf8mb4;port:3306';
 	$db_user = 'b814049c84b5dd';
 	$db_pass = '4edc2dd3';
 	$conexao = new PDO($dsn, $db_user, $db_pass); // abre conexão
